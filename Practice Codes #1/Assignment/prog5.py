bde50818dff3123aa60fb99f07a0424029e780fa
Bogs081105 <tubogkyrie0811@gmail.com>
2025-03-08 10:36:06 +0800

    Delete Practice Codes #1/Assignment directory

--- Practice Codes #1/Assignment/prog5.py
+++
@@ -1,6 +0,0 @@
-#Prog05: Create a program that ask user to input 2 numbers. Print the quotient of the two numbers with the decimal point
-num1 = float(input("Input a number: "))
-num2 = float(input("Input a second number: "))
-quotient = num1 / num2
-
-print(f"{num1}/{num2} = {quotient}")
--- Practice Codes #1/Assignment/prog7.py
+++
@@ -1,7 +0,0 @@
-#Prog07: Create a program that ask user to input 10 numbers. Print the sum of all the numbers.
-total = 0
-
-for i in range (10):
-    num = float(input(f"Enter number{i+1}:"))
-    total += num
-print (f"The total of the numbers are {total}")
